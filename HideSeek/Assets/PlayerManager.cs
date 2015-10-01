9d0e6b254012f00627112ee61f5e32704b7a8e46
unknown <arw2013@GDD-22.main.ad.rit.edu>
2015-10-01 13:23:08 -0400

    Added base/core files

---
+++ HideSeek/Assets/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PlayerManager : MonoBehaviour {
+
+	private MyNetworkManager netManager;
+	private bool isHider = false;
+
+	// Use this for initialization
+	void Start () {
+		netManager = GameObject.Find ("NetManager").GetComponent<MyNetworkManager>();
+		if (netManager.isHost) {
+			isHider = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && !isHider) {
+			print ("clicked");
+		}
+	}
+}
